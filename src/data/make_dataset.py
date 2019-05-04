630b4505ea70f3bdf5dc036c5658fbb2093cbc96
TcardLab <tcardlab@gmail.com>
2019-05-04 01:14:53 -0400

    Fleshing out data imports

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -28,3 +28,70 @@ if __name__ == '__main__':
     load_dotenv(find_dotenv())
 
     main()
+
+
+
+#rough draft for now...
+#(use inheritence for more tailored use later) or (set these as functions and dont us a class)?
+#probabaly overkill for what is likely to be static data anyway, oh well
+
+#run skempi.check_latest on regular basis
+#cron or crontab – https://desktop.arcgis.com/en/arcmap/10.4/analyze/executing-tools/scheduling-a-python-script-to-run-at-prescribed-times.htm
+class skempi: 
+    def __init__(self):
+        self.version = None
+        self.data = None
+        self.pdb = 'path(s) and status?' #store raw pdbs in raw
+
+    def __repr__(self):
+        if self.data== None:
+            return 'No data initialized'
+        if self.version== None:
+            return 'No data, hence no version'
+    
+
+    def check_version(self):
+        #try
+            #check if file exists get version from name
+        #except
+            #file missing...
+            #download new
+        pass
+        
+    def check_latest(self):
+        #if up to date
+            #pass
+        #else
+            #set path
+            #download new
+        pass
+    
+    def download(self):
+        #run wget from py?
+        #save.replace file as "skempi v#.#" to raw
+        #data=extract pandas dataframe
+        #wget pdb and send to raws 
+        pass
+
+
+#make more universal class and use inheritence... 
+class zemu: 
+    def __init__(self):
+        self.version = None
+        self.data = None  # make empty dataframe?
+        self.pdb = 'path(s) and status?' #
+
+    def check_existence(self):
+        #try
+            #data empty?
+            #do pdbs and folder exist?
+        #else:
+            #call download
+        pass
+    
+    def download(self):
+        #run wget from py?
+        #save.replace file as "skempi v#.#" to raw
+        #data=extract pandas dataframe
+        #wget pdb and send to raws 
+        pass

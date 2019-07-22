d13f569c4e99ccb83b4e46d1b61f1147d22d3035
TcardLab <tcardlab@gmail.com>
2019-07-22 02:42:03 -0400

    Format fix and file handling
    improved formatting. Some atypical commenting used as section headers, it makes these sections collapsable in IDE's.
    
    Deleted @click statements. It is extracomplexity and there is no need for CLI with custom file selection. Thus, paths are hard coded in the make_dataset.py file (easy to find).

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -12,8 +12,8 @@ import numpy as np
 def ChainCheck(df):
     '''
     Purpose:
-        Given multiple mutations, ensures they all occur within the
-        same set of chains that make up either protein.
+        Given multiple mutations, ensures they all occur within
+        the same set of chains that make up either protein.
 
     Output:
         crossChain <Bool>
@@ -21,86 +21,85 @@ def ChainCheck(df):
           • True: Cross-chain, mutations on both proteins
     '''
     if df['NumMutations'] == 1:
-    # Single mutant cannot be cross-chain
-      crossChain = False
-      return crossChain
+        # Single mutant cannot be cross-chain
+        crossChain = False
+        return crossChain
     else:
-    # Given multiple mutation profiles,
-      # Is the first mutated chain in potein 1 or 2
-      Chain = df['MutSplit'][0][1]
-      if Chain in df['Prot1Chain']:
-        ChainSet = df['Prot1Chain']
-      elif Chain in df['Prot2Chain']:
-        ChainSet = df['Prot2Chain']
-      # else: unexpected – ChainSet err
-      for mutation in df['MutSplit']:
-        Chain = mutation[1]  # update chain entry
-        if Chain in ChainSet:
-          crossChain = False
-        else:
-          # a mutation is on the other protein's chain
-          crossChain = True
-          break
+        # Given multiple mutation profiles,
+        # Is the first mutated chain in potein 1 or 2
+        Chain = df['MutSplit'][0][1]
+        if Chain in df['Prot1Chain']:
+            ChainSet = df['Prot1Chain']
+        elif Chain in df['Prot2Chain']:
+            ChainSet = df['Prot2Chain']
+        # else: unexpected – ChainSet err
+        for mutation in df['MutSplit']:
+            Chain = mutation[1]  # update chain entry
+            if Chain in ChainSet:
+                crossChain = False
+            else:
+                # a mutation is on the other protein's chain
+                crossChain = True
+                break
     return crossChain
 
 
 class MutantDataset(pd.DataFrame):
-  '''<Subclassed Pandsas DataFrame>
-     Given the potential of multiple sources for mutant datasets, 
-     this calss serves to improve clarity, debugging, and reusability
+    '''<Subclassed Pandsas DataFrame>
+        Given the potential of multiple sources for mutant datasets,
+        this calss serves to improve clarity, debugging, and reusability
 
-     Needs repr
-  '''
-
-  def __init__(self, data, sep=',', index=None, columns=None, dtype=None,
-               copy=True,):
-    '''Initialize subclass from DataFrame instance or csv path.'''
-    if type(data) == str:
-      data = pd.read_csv(data, sep=sep)
-    super(MutantDataset, self).__init__(data=data,
-                                        index=index,
-                                        columns=columns,
-                                        dtype=dtype,
-                                        copy=copy)
-
-  def Mutations(self, row):
-    '''Returns dictionary of mutation identifiers.'''
-    keys = ['initAA', 'chain', 'loc', 'mutAA']  # code key
-    mut_codes = self.loc[row]['Mutation(s)_cleaned'].split(',')
-    unzip_code = zip(*[re.findall('(\d+|.)', mut) for mut in mut_codes])
-    mut_dct = dict(zip(keys, unzip_code))
-    return mut_dct
-
-  def to_numeric(self, keys):
-    '''converts column of single or list of keys to numeric'''
-    self[keys] = self[keys].apply(pd.to_numeric, errors='coerce')
-    return self[keys]
-
-  def gibbsEq(self, Kd_key, tmp_key='Temperature'):
-    '''Gibbs Free Energy = -R * Temp * ln(kd)'''
-    R = 1.9872036e-3  # Ideal Gas Constant in kcal
-    ΔG = -R * self[tmp_key] * np.log(self[Kd_key])  # log is ln in np
-    return ΔG
-
-  def solve_ddG(self, wild, mutant, tmp_key='Temperature'):
-    '''ddG is the changes in affinity upon mutation:
-           ddG = dG_Mutant-dG_wild_Type
+        Needs repr
     '''
-    self['dgWT'] = self.gibbsEq(wild, tmp_key)
-    self['dgMut'] = self.gibbsEq(mutant, tmp_key)
-    self['ddG'] = self['dgWT']-self['dgMut']
-    return self
+    def __init__(self, data, sep=',', index=None, columns=None, dtype=None,
+                 copy=True,):
+        '''Initialize subclass from DataFrame instance or csv path.'''
+        if type(data) == str:
+            data = pd.read_csv(data, sep=sep)
+        super(MutantDataset, self).__init__(data=data,
+                                            index=index,
+                                            columns=columns,
+                                            dtype=dtype,
+                                            copy=copy)
 
-  def find_cross_chains(self):
-    '''checks if mutation occur on more than one protein'''
-    self['Prot1Chain'] = self['#Pdb'].str.split('_').str[1]
-    self['Prot2Chain'] = self['#Pdb'].str.split('_').str[2]
-    crossChain = self.apply(ChainCheck, axis=1)
-    return crossChain
+    def Mutations(self, row):
+        '''Returns dictionary of mutation identifiers.'''
+        keys = ['initAA', 'chain', 'loc', 'mutAA']  # code key
+        mut_codes = self.loc[row]['Mutation(s)_cleaned'].split(',')
+        unzip_code = zip(*[re.findall('(\d+|.)', mut) for mut in mut_codes])
+        mut_dct = dict(zip(keys, unzip_code))
+        return mut_dct
 
-  @property
-  def _constructor(self):
-    return MutantDataset  # Class Name
+    def to_numeric(self, keys):
+        '''converts column of single or list of keys to numeric'''
+        self[keys] = self[keys].apply(pd.to_numeric, errors='coerce')
+        return self[keys]
+
+    def gibbsEq(self, Kd_key, tmp_key='Temperature'):
+        '''Gibbs Free Energy = -R * Temp * ln(kd)'''
+        R = 1.9872036e-3  # Ideal Gas Constant in kcal
+        ΔG = -R * self[tmp_key] * np.log(self[Kd_key])  # log is ln in np
+        return ΔG
+
+    def solve_ddG(self, wild, mutant, tmp_key='Temperature'):
+        '''ddG is the changes in affinity upon mutation:
+            ddG = dG_Mutant-dG_wild_Type
+        '''
+        self['dgWT'] = self.gibbsEq(wild, tmp_key)
+        self['dgMut'] = self.gibbsEq(mutant, tmp_key)
+        self['ddG'] = self['dgWT']-self['dgMut']
+        return self
+
+    def find_cross_chains(self):
+        '''checks if mutation occur on more than one protein'''
+        self['Prot1Chain'] = self['#Pdb'].str.split('_').str[1]
+        self['Prot2Chain'] = self['#Pdb'].str.split('_').str[2]
+        crossChain = self.apply(ChainCheck, axis=1)
+        return crossChain
+
+    @property
+    def _constructor(self):
+        return MutantDataset  # Class Name
 
 
 def Clean_Skempi(path):
@@ -115,68 +114,70 @@ def Clean_Skempi(path):
     Output:
         SKEMPI_SingleSided : MutantDataset(pd.DataFrame)
     '''
-# Initialize class
-  skempi = MutantDataset(path, sep=';')
+  # Initialize class
+    skempi = MutantDataset(path, sep=';')
 
-# Convert non-numeric 'Temperature' comments to numeric values. Default is 298
-  skempi['Temperature'] = skempi['Temperature'].str.extract(r'(\d+)')
-  skempi['Temperature'] = skempi.to_numeric('Temperature')
-  skempi['Temperature'].fillna(value=298, inplace=True) #6665-6668 blank
-  
-# Calculate free energies
-  dropna_lst = ['Affinity_wt_parsed','Affinity_mut_parsed']
-  skempi.dropna(subset=dropna_lst, inplace=True)
-  skempi = skempi.solve_ddG('Affinity_wt_parsed', 'Affinity_mut_parsed')
+  # Convert 'Temperature' comments/str's to numeric values. Default is 298
+    skempi['Temperature'] = skempi['Temperature'].str.extract(r'(\d+)')
+    skempi['Temperature'] = skempi.to_numeric('Temperature')
+    skempi['Temperature'].fillna(value=298, inplace=True)  # 6665-6668 blank
 
-# Median and duplicate ddG/tmp values
-  group_keys = ['#Pdb', 'Mutation(s)_PDB']
-  skempi['ddgMedian'] = skempi.groupby(group_keys)['ddG'].transform('median')
-  skempi = skempi.drop_duplicates(subset=[*group_keys,'Temperature'],
-                                  keep='first', inplace=False)
+  # Calculate free energies
+    dropna_lst = ['Affinity_wt_parsed', 'Affinity_mut_parsed']
+    skempi.dropna(subset=dropna_lst, inplace=True)
+    skempi = skempi.solve_ddG('Affinity_wt_parsed', 'Affinity_mut_parsed')
 
-# Flag multiple mutations in the same protein
-  skempi['MutSplit'] = skempi['Mutation(s)_PDB'].str.split(',')
-  skempi['NumMutations'] = skempi['MutSplit'].apply(len)
+  # Median and duplicate ddG/tmp values
+    group_keys = ['#Pdb', 'Mutation(s)_PDB']
+    skempi['ddgMedian'] = skempi.groupby(group_keys)['ddG'].transform('median')
+    skempi = skempi.drop_duplicates(subset=[*group_keys, 'Temperature'],
+                                    keep='first', inplace=False)
 
-# Extract Chains and remove cross chain mutations. 
-  skempi['CrossChain'] = skempi.find_cross_chains()
-  SKEMPI_SingleSided = skempi[skempi.CrossChain == False]
-  return SKEMPI_SingleSided
+  # Flag multiple mutations in the same protein
+    skempi['MutSplit'] = skempi['Mutation(s)_PDB'].str.split(',')
+    skempi['NumMutations'] = skempi['MutSplit'].apply(len)
+
+  # Extract Chains and remove cross chain mutations.
+    skempi['CrossChain'] = skempi.find_cross_chains()
+    SKEMPI_SingleSided = skempi[skempi.CrossChain == False]
+    return SKEMPI_SingleSided
 
 
-@click.command()
-# http://click.palletsprojects.com/en/5.x/arguments/#file-arguments
-@click.argument('input_filepath', type=click.Path(exists=True)) # must exist
-@click.argument('interim_filepath', type=click.Path()) # will create
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, interim_filepath, output_filepath):
+# @click commands removed, extra complexity and CLI unnecessary ATM
+def main():
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
 
         1) clean each dataset to create consistant MutantDataset's
-            1a) store indeviduals in ~/data/intermediate 
-        2) combine into uniform MutantDataset 
-            2a) store in ~/data/final  
+            1a) store indeviduals in ~/data/intermediate
+        2) combine into uniform MutantDataset
+            2a) store in ~/data/final
     """
-# 1.1 – Clean skempi
+  # Paths
+    input_filepath = 'data/raw/'
+    interim_filepath = 'data/interim/'
+    output_filepath = 'data/processed/'
+
+  # 1.0 – Clean skempi
     skempi_final = Clean_Skempi(input_filepath + 'skempi_2.0.csv')
     # Log Info
     NumProteins = skempi_final['#Pdb'].nunique()
     NumMutations = skempi_final['#Pdb'].count()
     print(f'There are {NumMutations} unique single sided'
           f'mutations in {NumProteins} proteins')
-# 1.1a save intermediate to interim
-    skempi_final.to_csv('~/data/interim')
+  # 1.0a save intermediate to interim
+    skempi_final.to_csv(interim_filepath + 'skempi_final.csv')
 
-# 1.2 – Import Other
+  # 1.1 – Import Other
     # other_final = Clean_Other(input_filepath + 'other.csv')
-# 1.2a – save intermediate to intermediates
-    # Other_final.to_csv('~/data/intermediate')
+  # 1.1a – save intermediate to intermediates
+    # Other_final.to_csv('data/intermediate')
 
-# 2 – Combine datasets
+  # 2 – Combine datasets
     # code
-# 2a – save final to processed
-    # combined.to_csv(~/data/processed')
+  # 2a – save final to processed
+    # combined.to_csv(data/processed')
+    skempi_final.to_csv(output_filepath + 'skempi_final.csv')
 
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
@@ -193,4 +194,4 @@ if __name__ == '__main__':
     # load up the .env entries as environment variables
     load_dotenv(find_dotenv())
 
-    main('data/raw/', 'data/interim/', 'data/processed/')
+    main()
